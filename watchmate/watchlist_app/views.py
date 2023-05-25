35544a10b5ef6524b7e78cfb87ea8fc322a96ea7
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2023-05-25 13:21:53 +0530

    added an API function returning all movies, in JSON format

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
 
 # Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    print(movies.values())
+
+    return JsonResponse({'movies': list(movies.values())})
